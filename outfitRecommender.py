268b81f79701c70fead280de8b7b8c94a702d1e2
Nynx4life <jakob-fehle@online.de>
2017-08-17 17:33:42 +0200

    Loading all images + computing avg colors

--- outfitRecommender.py
+++ outfitRecommender.py
@@ -2,39 +2,50 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import os
+from os import listdir
 import Tkinter as tk
 import cv2
 
 # Einlesen Bilder
-img2 = cv2.imread('data/Hose/7-8-denim-jeans-weiss-street-one.jpg')
-img = cv2.imread('data/Hose/114864f5c2af959cb4dddead5a4dc6c1.jpg')
-print(img)
+images = []
+imagesAvgCol = []
+scriptPath = os.path.dirname(__file__)
+filenames = listdir(os.path.join(scriptPath,'data/Hose'))
+print(filenames)
+for img in filenames:
+    images.append(cv2.imread(os.path.join(scriptPath,'data/Hose',img)))
 
 
-# Zählt keine Weißen Pixel (Hintergrund) mit
-totalR = 0
-totalG = 0
-totalB = 0
-totalPixel = 0
+for img in images:
+    # Zählt keine Weißen Pixel (Hintergrund) mit
+    totalR = 0
+    totalG = 0
+    totalB = 0
+    totalPixel = 0
 
-for row in img:
-    for pixel in row:
-        if pixel[0] > 245 and pixel[1]  > 245 and pixel[2] > 245:
-            continue
-        else:
-            totalR = totalR + pixel[0]
-            totalG = totalG + pixel[1]
-            totalB = totalB + pixel[2]
-            totalPixel = totalPixel +1
+    for row in img:
+        for pixel in row:
+            if pixel[0] > 245 and pixel[1]  > 245 and pixel[2] > 245:
+                continue
+            else:
+                totalR = totalR + pixel[0]
+                totalG = totalG + pixel[1]
+                totalB = totalB + pixel[2]
+                totalPixel = totalPixel +1
 
-avgPixel = []
-avgPixel.append(totalR / totalPixel)
-avgPixel.append(totalG / totalPixel)
-avgPixel.append(totalB / totalPixel)
+    avgPixel = []
+    avgPixel.append(totalR / totalPixel)
+    avgPixel.append(totalG / totalPixel)
+    avgPixel.append(totalB / totalPixel)
 
-print(avgPixel)
+    print(avgPixel)
+    imagesAvgCol.append(avgPixel)
+    
+    # Zählt weiße Pixel mit
+    average_color_per_row = np.average(img, axis=0)
+    average_color = np.average(average_color_per_row, axis=0)
+    print(average_color)
 
-# Zählt weiße Pixel mit
-average_color_per_row = np.average(img, axis=0)
-average_color = np.average(average_color_per_row, axis=0)
-print(average_color)
+
+print imagesAvgCol
