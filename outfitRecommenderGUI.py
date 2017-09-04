7ad012b4e7d2226bac011afe6cdec03f65406772
Josef Reiml <josef.reiml@stud.uni-regensburg.de>
2017-09-05 01:52:11 +0200

    Code optimisation, the GUI now only uses the grid to place buttons (before it was a mix of grid and pack)
    The button placements can now be manipulated with grid weight and sticky

--- outfitRecommenderGUI.py
+++ outfitRecommenderGUI.py
@@ -8,47 +8,59 @@ import outfitRecommender
 
 
 class OutfitRecommenderGUI(tk.Frame):
-    buttonBackground = "#000000"
-    fieldWidth = 25
+    # size of GUI related elements
+    windowSize = [1366, 768]
+    buttonSize = 160
+
     # used for dynamically accessing the four cloth lists
     filenamePrefixes = ["data/top/", "data/jacket/", "data/trousers/", "data/shoes/"]
     clothNames = ['top', 'jacket', 'trousers', 'shoes']
+
+    # used to save all the default button images
     buttonImages = {}
+
+    # used to save the recommended images, they won't be shown otherwise // dirty workaround
+    recommendedImages = {}
 
     def __init__(self, root):
         tk.Frame.__init__(self, root)
         self.root = root
+        self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
 
         # Initializing the buttons representing the cloth pieces
-        for j in range(len(outfitRecommender.filenames)):
-            for k in range(len(outfitRecommender.filenames[j])):
-                self.original = Image.open(self.filenamePrefixes[j] + outfitRecommender.filenames[j][k])
-                resized = self.original.resize((160, 160), Image.ANTIALIAS)
-                self.buttonImages[(j, k)] = ImageTk.PhotoImage(resized)
-
-        # The first four cloth pieces to be shown // buttons need to safe the path to the image for correct behaviour
-        self.startScreenButtons = [self.buttonImages[(0, 0)], self.buttonImages[(1, 0)], self.buttonImages[(2, 0)],
-                                   self.buttonImages[(3, 0)]]
+        self.__initButtonImages__()
 
         # list for saving selected Outfit // unnecessary when the planed structure works out
-        self.selectedPieces = []
+        # self.selectedPieces = []
 
-        # counts the number of pieces selected by the user // terminate the app when the last piece is selected
-        self.piecesSelected = 0  # ?
-
-        # overrides the behaviour of the X-button, the user now has to confirm the closing of the application
-        self.root.protocol("WM_DELETE_WINDOW", self.on_quit)
+        # counts the number of pieces selected by the user // show dialog when the last piece is selected
+        self.piecesSelected = 0
 
         # contains every button shown in the frame
         self.buttons = {}
 
         self.__initUI__()
 
+    # Initializes the buttons representing the cloth pieces
+    def __initButtonImages__(self):
+        for j in range(len(outfitRecommender.filenames)):
+            for k in range(len(outfitRecommender.filenames[j])):
+                self.original = Image.open(self.filenamePrefixes[j] + outfitRecommender.filenames[j][k])
+                resized = self.original.resize((self.buttonSize, self.buttonSize), Image.ANTIALIAS)
+                self.buttonImages[(j, k)] = ImageTk.PhotoImage(resized)
+
+        # The first four cloth pieces to be shown // buttons need to safe the path to the image for correct behaviour
+        self.startScreenButtons = [self.buttonImages[(0, 0)], self.buttonImages[(1, 0)], self.buttonImages[(2, 0)],
+                                   self.buttonImages[(3, 0)]]
+
     # sets up the window and its menu-bar
     def __initUI__(self):
         # size of a 720p display
-        self.root.minsize(width=1366, height=768)
+        self.root.minsize(width=self.windowSize[0], height=self.windowSize[1])
         self.root.title("Outfit-Recommender")
+
+        # overrides the behaviour of the X-button, the user now has to confirm the closing of the application
+        self.root.protocol("WM_DELETE_WINDOW", self.on_quit)
 
         menu_main = tk.Menu(self.root)
         menu_file = tk.Menu(menu_main, tearoff=0)
@@ -70,48 +82,51 @@ class OutfitRecommenderGUI(tk.Frame):
 
         self.root.config(menu=menu_main)
 
+    def on_quit(self):
+        if tkMessageBox.askokcancel("Quit", "Do you really wish to quit?"):
+            root.destroy()
+
     def new_recommendation(self, row, col):
+        self.remove_all()
+        self.__resize__()
+        self.init_grid(row, col)
+
+    def remove_all(self):
         # clears the frame without destroying the frame itself:
         # http://stackoverflow.com/questions/15781802/python-tkinter-clearing-a-frame
         for widget in self.winfo_children():
             widget.destroy()
 
+        # removes all buttons form the list
         self.buttons.clear()
-        self.__resize__(col * self.fieldWidth, row * self.fieldWidth)
-        self.init_grid(row, col)
 
-    # private function for window resizing // can only increase size?! -> needs testing?/doesnt matter?
-    def __resize__(self, width, height):
-        root.geometry('{}x{}'.format(width, height))
+    # private function for keeping the size of the window // will be obsolete when show_all uses multiple rows
+    def __resize__(self):
+        root.geometry('{}x{}'.format(self.windowSize[0], self.windowSize[1]))
 
     # initializes grid UI and fills it with the cloth-buttons
     def init_grid(self, row, col):
         for j in range(row):
             for k in range(col):
-                self.buttons[(j, k)] = tk.Button(self, width=160, height=160, image=self.startScreenButtons[j],
+                self.buttons[(j, k)] = tk.Button(self, width=self.buttonSize, height=self.buttonSize,
+                                                 image=self.startScreenButtons[j],
                                                  state="normal")
                 b = self.buttons[(j, k)]
                 b.grid(row=j, column=k, padx=(10, 10), pady=(10, 10))
                 # Left-Click
                 b.bind('<Button-1>', lambda event, x=j, y=k: self.show_all(x))
 
-        self.pack()
-
     # needs to remove all buttons currently in the frame
     def show_all(self, cloth_name_index):
+        self.remove_all()
         for k in range(len(outfitRecommender.filenames[cloth_name_index])):
-            self.buttons[(cloth_name_index, k)] = tk.Button(self, width=160, height=160,
+            self.buttons[(cloth_name_index, k)] = tk.Button(self, width=self.buttonSize, height=self.buttonSize,
                                                             image=self.buttonImages[(cloth_name_index, k)],
                                                             state="normal")
             b = self.buttons[(cloth_name_index, k)]
             b.grid(row=cloth_name_index, column=k, padx=(10, 10), pady=(10, 10))
             # Left-Click
             b.bind('<Button-1>', lambda event, x=cloth_name_index, y=k: self.show_recommendations(x, y))
-
-        self.pack()
-
-    # used to save the recommended images, they won't be shown otherwise // dirty workaround
-    recommendedImages = {}
 
     def show_recommendations(self, r, c):
         if r != 3:
@@ -125,21 +140,15 @@ class OutfitRecommenderGUI(tk.Frame):
         print (next_cloth)
         for k in range(len(next_cloth)):
             self.original = Image.open(self.filenamePrefixes[r] + next_cloth[k][0])
-            resized = self.original.resize((160, 160), Image.ANTIALIAS)
+            resized = self.original.resize((self.buttonSize, self.buttonSize), Image.ANTIALIAS)
             self.recommendedImages[(r, k)] = ImageTk.PhotoImage(resized)
-            self.buttons[(r, k)] = tk.Button(self, width=160, height=160,
+            self.buttons[(r, k)] = tk.Button(self, width=self.buttonSize, height=self.buttonSize,
                                              image=self.recommendedImages[(r, k)],
                                              state="normal")
             b = self.buttons[(r, k)]
             b.grid(row=r, column=k, padx=(10, 10), pady=(10, 10))
             # Left-Click
             b.bind('<Button-1>', lambda event, x=r, y=k: self.show_recommendations(x, y))
-
-        self.pack()
-
-    def on_quit(self):
-        if tkMessageBox.askokcancel("Quit", "Do you really wish to quit?"):
-            root.destroy()
 
 
 root = tk.Tk()
